f62ad580fec507d749703cf8b557334910bfcc98
LenSin3 <lenpk3@yahoo.co.uk>
2020-12-09 13:38:40 -0800

    add svg wrapper

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,18 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
+// Step 2: Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+// =================================
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+
