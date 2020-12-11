8107016024949796151d346b00d5cac6120b75d2
LenSin3 <lenpk3@yahoo.co.uk>
2020-12-10 20:56:20 -0800

    Added a wrapper to the chart code to make it responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,161 +1,186 @@
-// @TODO: YOUR CODE HERE!
-// Step 1: Set up our chart
-//= ================================
-var svgWidth = 960;
-var svgHeight = 500;
+// Wrap chart code inside a function
+// that automatically resizes the chart
+function makeResponsive() {
 
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 50
-};
+  // if the SVG area isn't empty when the browser loads, remove it
+  // and replace it with a resized version of the chart
+  var svgArea = d3.select("body").select("svg");
+  if (!svgArea.empty()) {
+    svgArea.remove();
+  }
 
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+   // SVG wrapper dimensions are determined by the current width
+  // and height of the browser window.
+  var svgWidth = window.innerWidth;
+  var svgHeight = window.innerHeight;
+  // @TODO: YOUR CODE HERE!
+  // Step 1: Set up our chart
+  //= ================================
+  // var svgWidth = 960;
+  // var svgHeight = 500;
 
-// Step 2: Create an SVG wrapper,
-// append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
-// =================================
-var svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+  var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 50
+  };
 
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  var width = svgWidth - margin.left - margin.right;
+  var height = svgHeight - margin.top - margin.bottom;
 
-// Step 3:
-// Import data from data.csv file
-// ================================
-d3.csv("assets/data/data.csv").then(function(censusData) {
-    console.table(censusData),
-    // Step 4: Parse the data
-    // Use age and smoing data for our chart
-    censusData.forEach(function(data) {
-        data.age = +data.age;
-        data.smokes = +data.smokes;        
-    });
+  // Step 2: Create an SVG wrapper,
+  // append an SVG group that will hold our chart,
+  // and shift the latter by left and top margins.
+  // =================================
+  var svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
-    // Step 5: Create Scales
-    //= ============================================
-    // x scale to scale ages data
-    var xLinearScale = d3.scaleLinear()
-    .domain(d3.extent(censusData, data => data.age))
-    .range([0, width]);
+  var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    // y scale to scale smokes data
-    var yLinearScale = d3.scaleLinear()
-    .domain(d3.extent(censusData, data => data.smokes))
-    .range([height, 0]);
+  // Step 3:
+  // Import data from data.csv file
+  // ================================
+  d3.csv("assets/data/data.csv").then(function(censusData) {
+      console.table(censusData),
+      // Step 4: Parse the data
+      // Use age and smoing data for our chart
+      censusData.forEach(function(data) {
+          data.age = +data.age;
+          data.smokes = +data.smokes;        
+      });
 
-    // Step 6: Create Axes
-    // =============================================
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
+      // Step 5: Create Scales
+      //= ============================================
+      // x scale to scale ages data
+      var xLinearScale = d3.scaleLinear()
+      .domain(d3.extent(censusData, data => data.age))
+      .range([0, width]);
 
-    // Step 7: Append the axes to the chartGroup - ADD STYLING
-    // ==============================================
-    // Add bottomAxis
-    chartGroup.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
+      // y scale to scale smokes data
+      var yLinearScale = d3.scaleLinear()
+      .domain(d3.extent(censusData, data => data.smokes))
+      .range([height, 0]);
 
-    // left axis
-    chartGroup.append("g")
-    // .attr("stroke", "green") // NEW!
-    // .classed("green", true)
-    .call(leftAxis);
+      // Step 6: Create Axes
+      // =============================================
+      var bottomAxis = d3.axisBottom(xLinearScale);
+      var leftAxis = d3.axisLeft(yLinearScale);
 
-    
-    // append circles
-   /* var circlesGroup = chartGroup.selectAll("circle")
+      // Step 7: Append the axes to the chartGroup - ADD STYLING
+      // ==============================================
+      // Add bottomAxis
+      chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+      // left axis
+      chartGroup.append("g")
+      // .attr("stroke", "green") // NEW!
+      // .classed("green", true)
+      .call(leftAxis);
+
+      
+      // append circles
+    /* var circlesGroup = chartGroup.selectAll("circle")
+        .data(censusData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.age))
+        .attr("cy", d => yLinearScale(d.smokes))
+        .attr("r", "8")
+        //.classed("chart", true)
+        .classed("stateCircle", true);
+        //.classed("stateText", function((d, i) {
+        // d.abbr;
+        //})
+      // Append text element to the circles
+      
+      var textGroup = chartGroup.selectAll("text")
       .data(censusData)
       .enter()
-      .append("circle")
-      .attr("cx", d => xLinearScale(d.age))
-      .attr("cy", d => yLinearScale(d.smokes))
-      .attr("r", "8")
-      //.classed("chart", true)
-      .classed("stateCircle", true);
-      //.classed("stateText", function((d, i) {
-       // d.abbr;
-      //})
-    // Append text element to the circles
-    
-    var textGroup = chartGroup.selectAll("text")
-    .data(censusData)
-    .enter()
-    .append("text");
+      .append("text");
 
-    var textLabel = textGroup
-      // .attr("dx", 0)
-      // .attr("dy", ".35em")
+      var textLabel = textGroup
+        // .attr("dx", 0)
+        // .attr("dy", ".35em")
+        .attr("dx", d => xLinearScale(d.age))
+        .attr("dy", d => yLinearScale(d.smokes))
+        .attr("font-size", "5px")
+        .text(function(d) {
+          return d.abbr;
+        })
+        .classed("stateText", true);*/
+
+      /*chartGroup.append("text")
+      .text(function(d) {
+        return d.abbr;
+      })
       .attr("dx", d => xLinearScale(d.age))
       .attr("dy", d => yLinearScale(d.smokes))
-      .attr("font-size", "5px")
-      .text(function(d) {
-        return d.abbr;
-      })
+      .attr("font-size", "10px")
       .classed("stateText", true);*/
 
-    /*chartGroup.append("text")
-    .text(function(d) {
-      return d.abbr;
-    })
-    .attr("dx", d => xLinearScale(d.age))
-    .attr("dy", d => yLinearScale(d.smokes))
-    .attr("font-size", "10px")
-    .classed("stateText", true);*/
-
-    // append circles
-    var circlesGroup = chartGroup.selectAll("circle")
+      // append circles
+      var circlesGroup = chartGroup.selectAll("circle")
+        .data(censusData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.age))
+        .attr("cy", d => yLinearScale(d.smokes))
+        .attr("r", "15")
+        //.classed("chart", true)
+        .classed("stateCircle", true);
+        //.classed("stateText", function((d, i) {
+        // d.abbr;
+        //})
+      // Append text element to the circles
+      
+      var textGroup = chartGroup.selectAll(null)
       .data(censusData)
       .enter()
-      .append("circle")
-      .attr("cx", d => xLinearScale(d.age))
-      .attr("cy", d => yLinearScale(d.smokes))
-      .attr("r", "15")
-      //.classed("chart", true)
-      .classed("stateCircle", true);
-      //.classed("stateText", function((d, i) {
-       // d.abbr;
-      //})
-    // Append text element to the circles
-    
-    var textGroup = chartGroup.selectAll(null)
-    .data(censusData)
-    .enter()
-    .append("text");
+      .append("text");
 
-    var textLabel = textGroup
-      // .attr("dx", 0)
-      // .attr("dy", ".35em")
-      .attr("x", d => xLinearScale(d.age))
-      .attr("y", d => yLinearScale(d.smokes))
-      //.attr("dx", ".71em")
-      //.attr("dy", ".35em")
-      .attr("font-size", "10px")
-      .text(function(d) {
-        return d.abbr;
-      })
-      .classed("stateText", true);
-      
-   
+      var textLabel = textGroup
+        // .attr("dx", 0)
+        // .attr("dy", ".35em")
+        .attr("x", d => xLinearScale(d.age))
+        .attr("y", d => yLinearScale(d.smokes))
+        //.attr("dx", ".71em")
+        //.attr("dy", ".35em")
+        .attr("font-size", "10px")
+        .text(function(d) {
+          return d.abbr;
+        })
+        .classed("stateText", true);
+        
     
       
+        
 
-
- 
 
   
 
+    
 
-}).catch(function(error) {
-    console.log(error);
-});
+
+  }).catch(function(error) {
+      console.log(error);
+  });
+
+};
+
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, responsify() is called.
+d3.select(window).on("resize", makeResponsive);
+
+
 
 
 
